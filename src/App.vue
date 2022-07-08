6e8713e2251dabe37dd4433f10e2f1be6f768cff
shj1995 <517925985@qq.com>
2022-07-08 16:43:31 +0800

    add global style

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,29 @@
 <template>
+  <van-config-provider :theme-vars="themeVars">
   <router-view />
+  </van-config-provider>
 </template>
-<script setup lang="ts"></script>
+<script  lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  name: '',
+  setup() {
+    const rate = ref(4);
+    const slider = ref(50); // themeVars 内的值会被转换成对应 CSS 变量
+    // 比如 sliderBarHeight 会转换成 `--van-slider-bar-height`
+    const themeVars = {
+      rateIconFullColor: '#ffa500',
+      sliderBarHeight: '4px',
+      sliderButtonWidth: '20px',
+      sliderButtonHeight: '20px',
+      sliderActiveBackgroundColor: '#ffa500',
+      buttonPrimaryBorderColor: '#ffa500',
+      buttonPrimaryBackgroundColor: '#ffa500',
+    };
+    return {
+      rate,
+      slider,themeVars};
+  },
+});
+</script>
 <style></style>
