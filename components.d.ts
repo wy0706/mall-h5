e17aee6824df143b1b08e3d9de9d55838b82fbff
shj1995 <517925985@qq.com>
2022-07-08 16:43:49 +0800

    add global style

--- components.d.ts
+++ components.d.ts
@@ -7,10 +7,14 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     VanButton: typeof import('vant/es')['Button']
+    VanConfigProvider: typeof import('vant/es')['ConfigProvider']
+    VanField: typeof import('vant/es')['Field']
+    VanForm: typeof import('vant/es')['Form']
+    VanRate: typeof import('vant/es')['Rate']
+    VanSlider: typeof import('vant/es')['Slider']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
   }
